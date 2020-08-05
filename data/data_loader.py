fe7dc507bedbc7bc6ff5b2bef13a579bb8d14129
Maggie_AKI <maggie.zhu2016@outlook.com>
2020-08-05 22:17:15 +1000

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -22,6 +22,144 @@ class DataLoader():
     def _load(self):
         pass
 
+class CamVidLoader(DataLoader):
+    def __init__(self, img_path, label_path, test_num = 200):
+        self.label_names = ['animal','archway','bicyclist','bridge','building','car','pram','child','column',
+                            'fence','drive','lane','text','scooter','others','parking','pedestrian','road','shoulder',
+                            'sidewalk','sign','sky','suv','traffic cone','traffic light','train','tree','truck',
+                            'tunnel', 'vegetation','void','wall']
+        super().__init__(img_path, label_path)
+        self.test_num = test_num
+        self.class_num = 32
+
+        self.test_data = self.generate_testing_samples()
+
+        # a data-loading class for SiftFlow data
+        
+    def _load(self):
+        for root, dirs, files in os.walk(self.img_p):
+            for f in sorted(files):
+                if f.endswith('.png'):
+                    fid = f.split('.')[0]
+                    self.img_ids.append(fid)
+        self.ids = np.arange(len(self.img_ids))
+        return len(self.img_ids)
+
+    # rule: load enough images that could provide k shots for each class
+    def check_datatset_completence(self, counts, k):
+        count = 0
+        for each in counts:
+            if each < k:
+                return False
+            count += 1
+        return True
+
+    def separate_labels(self, lb, nc):
+        seg_lb = np.zeros((lb.shape[0], lb.shape[1], nc))
+        for i in range(nc):
+            seg_lb[:,:,i] = (lb==i).astype(int)
+        return seg_lb
+
+    def generate_supervised_samples(self,  
+                                  k = 1,   # k - number of shots for each class
+                                  img_height = 256,
+                                  img_width = 256):
+        while True:   
+            # randomly choose a batch of k images
+            yield (self._generate_train(k, img_height, img_width))
+
+    def generate_testing_samples(self, k = 1, 
+                                 img_height = 256,
+                                 img_width = 256):
+        images = []
+        labels = []
+
+        counts = np.zeros(self.class_num)
+
+        for fid in random.sample(self.img_ids, len(self.img_ids)):
+            if self.check_datatset_completence(counts, k):
+                break
+            # print('Loading image of {0}'.format(fid))
+            
+            # load label
+            flabel_path = fid + '_L.png'
+            flpath = os.path.join(self.label_p, flabel_path)
+            flabel = resize(imread(flpath), (img_height, img_width)).astype(int)[:,:,0]
+            flabel_classes = np.unique(flabel)
+            flabel = self.separate_labels(flabel, self.class_num)
+
+            flag = 0
+            for i in range(self.class_num):
+                if counts[i] < k and i in flabel_classes:
+                    counts[i] += 1
+                    
+                    if flag == 0:
+                        # load img
+                        fimg_path = fid + '.png'
+                        fpath = os.path.join(self.img_p, fimg_path)
+                        fimg = resize(imread(fpath)/255, (img_height, img_width))
+                        images.append(fimg)
+                        labels.append(flabel)
+                        flag = 1
+
+        images = np.array(images)
+        labels = np.array(labels)
+        # print(images.shape)
+        return images, labels
+
+    def generate_real_samples(self, dataset, n_samples, seed = None):
+        imgs, labels = dataset
+        if seed:
+            np.random.seed(seed)
+        # choose random instances
+        idx = randint(0, imgs.shape[0], n_samples)
+        # load images and labels
+        x, y = imgs[idx], labels[idx]
+        # generate class labels
+        y2 = np.ones([n_samples, x.shape[1], x.shape[2]])
+        return [x, y], y2
+
+    # generate labeled training dataset
+    def _generate_train(self, k, img_height, img_width): 
+        # load images and corresponding labels
+        # ids = self.ids[:-self.test_num]
+        images = []
+        labels = []
+
+        counts = np.zeros(self.class_num)
+
+        for fid in random.sample(self.img_ids, len(self.img_ids)):
+            if self.check_datatset_completence(counts, k):
+                break
+            # print('Loading image of {0}'.format(fid))
+            
+            # load label
+            flabel_path = fid + '_L.png'
+            flpath = os.path.join(self.label_p, flabel_path)
+            flabel = resize(imread(flpath), (img_height, img_width)).astype(int)[:,:,0]
+            flabel_classes = np.unique(flabel)
+            flabel = self.separate_labels(flabel, self.class_num)
+
+            flag = 0
+            for i in range(self.class_num):
+                if counts[i] < k and i in flabel_classes:
+                    counts[i] += 1
+                    
+                    if flag == 0:
+                        # load img
+                        fimg_path = fid + '.png'
+                        fpath = os.path.join(self.img_p, fimg_path)
+                        fimg = resize(imread(fpath)/255, (img_height, img_width))
+                        images.append(fimg)
+                        labels.append(flabel)
+                        flag = 1
+
+        images = np.array(images)
+        labels = np.array(labels)
+        # print(images.shape)
+        return images, labels
+
+
 # a data-loading class for SiftFlow data
 class SiftFlowLoader(DataLoader):
     def __init__(self, img_path, label_path, test_num = 200):
